0702ded9722f7d2a7800544ccf5f8cb5c932c93c
HermesonxBerp <hermesonsantos@berp.com.br>
2024-11-21 23:43:07 -0300

    integrating the signUp and SignIn methods

---
+++ src/assets/js/signIn.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.querySelector("form");
+
+    form.addEventListener("submit", async (event) => {
+        event.preventDefault();
+
+        const email = document.getElementById("email").value;
+
+        try {
+            const response = await axios.post("http://localhost:3000/users/checkUser/", {email});
+
+            console.log(response)
+
+            if (response.status === 200) {
+                alert("Usuário logado com sucesso!");
+                window.location.href = "../pages/home.html";
+                form.reset();
+            } else {
+                alert("Erro ao logar usuário.");
+            }
+        } catch (error) {
+            console.error("Erro na requisição:", error);
+            alert("Erro ao conectar ao servidor. Tente novamente mais tarde.");
+        }
+    });
+});
---
+++ src/assets/js/signUp.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.querySelector("form");
+
+    form.addEventListener("submit", async (event) => {
+        event.preventDefault();
+
+        const name = document.getElementById("name").value;
+        const email = document.getElementById("email").value;
+        const password = document.getElementById("password").value;
+
+        const data = { name, email, password, type: "Aluno" };
+
+        try {
+            const response = await axios.post("http://localhost:3000/users/create/", data);
+
+            console.log(response)
+
+            if (response.status === 201) {
+                alert("Usuário criado com sucesso!");
+                window.location.href = "../pages/home.html";
+                form.reset();
+            } else {
+                alert("Erro ao criar usuário.");
+            }
+        } catch (error) {
+            console.error("Erro na requisição:", error);
+            alert("Erro ao conectar ao servidor. Tente novamente mais tarde.");
+        }
+    });
+});
